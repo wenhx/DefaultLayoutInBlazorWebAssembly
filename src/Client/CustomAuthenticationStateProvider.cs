2b9c792dcaa241815282cc25396214011920125a
wenhx <3909713+wenhx@users.noreply.github.com>
2023-05-19 00:43:01 +0800

    Submit code that demonstrates the problem.

---
+++ src/Client/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+internal class CustomAuthenticationStateProvider : AuthenticationStateProvider
+{
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var state = new AuthenticationState(BuildNotAuthenticatedUser());
+        //var state = new AuthenticationState(BuildAuthenticatedUser());
+        return Task.FromResult(state);
+    }
+
+    ClaimsPrincipal BuildAuthenticatedUser()
+    {
+        var claims = new List<Claim>
+        {
+           new Claim(ClaimTypes.Name, "Admin"),
+           new Claim(ClaimTypes.Email, "admin@test.com"),
+           new Claim(ClaimTypes.Role, "Administrators")
+        };
+        var identity = new ClaimsIdentity(claims, "TokenAuthentication");
+        var principal = new ClaimsPrincipal(identity);
+        return principal;
+    }
+
+    ClaimsPrincipal BuildNotAuthenticatedUser()
+    {
+        var identity = new ClaimsIdentity();
+        var principal = new ClaimsPrincipal(identity);
+        return principal;
+    }
+}
---
+++ src/Client/Program.cs
@@ -0,0 +1,18 @@
+using BlazorApp_empty1.Client;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("BlazorApp_empty1.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorApp_empty1.ServerAPI"));
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+
+await builder.Build().RunAsync();
